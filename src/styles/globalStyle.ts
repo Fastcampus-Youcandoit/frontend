b0c18e86d61684c18a8bca604605cdfb32eb7c5b
Eojoonhyuk <anftlcb@gmail.com>
2023-09-11 22:39:48 +0900

    docs: index.ts파일 삭제

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components/dist/base";
+import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
