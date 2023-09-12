a62423e1610c495e18ce64da4dd274aaacde6e32
Eojoonhyuk <anftlcb@gmail.com>
2023-09-12 21:25:46 +0900

    design: add Header layout

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import logo from "../../assets/icons/header_icon/logo.png";
+
+const LogoBox = styled.a`
+  width: 150px;
+  height: 50%;
+`;
+
+const LogoImg = styled.img`
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+`;
+
+const Logo = () => {
+  return (
+    <LogoBox>
+      <LogoImg src={logo} alt="youcandoit logo" />
+    </LogoBox>
+  );
+};
+
+export default Logo;
