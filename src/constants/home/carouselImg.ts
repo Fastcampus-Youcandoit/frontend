69be69bc377944d89588ba6504a8c65a0eb3077e
Eojoonhyuk <anftlcb@gmail.com>
2023-09-13 20:52:47 +0900

    add carousel image data

---
+++ src/constants/home/carouselImg.ts
@@ -0,0 +1,41 @@
+import img1 from "../../assets/images/img1.png";
+import img2 from "../../assets/images/img2.png";
+import img3 from "../../assets/images/img3.png";
+import img4 from "../../assets/images/img4.png";
+import img5 from "../../assets/images/img5.png";
+import img6 from "../../assets/images/img6.png";
+
+const carouselImageData = [
+  {
+    label: "Image 1",
+    alt: "imgage 1",
+    src: img1,
+  },
+  {
+    label: "Image 2",
+    alt: "imgage 2",
+    src: img2,
+  },
+  {
+    label: "Image 3",
+    alt: "imgage 3",
+    src: img3,
+  },
+  {
+    label: "Image 4",
+    alt: "imgage 4",
+    src: img4,
+  },
+  {
+    label: "Image 5",
+    alt: "imgage 5",
+    src: img5,
+  },
+  {
+    label: "Image 6",
+    alt: "imgage 6",
+    src: img6,
+  },
+];
+
+export default carouselImageData;
