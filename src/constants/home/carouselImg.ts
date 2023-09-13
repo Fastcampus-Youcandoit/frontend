279056acdbea5489a52a9930b7c4fe66944a6238
Eojoonhyuk <anftlcb@gmail.com>
2023-09-14 00:00:02 +0900

    remove: change carousel image

--- src/constants/home/carouselImg.ts
+++ src/constants/home/carouselImg.ts
@@ -1,41 +1,15 @@
-import img1 from "../../assets/images/img1.png";
-import img2 from "../../assets/images/img2.png";
-import img3 from "../../assets/images/img3.png";
-import img4 from "../../assets/images/img4.png";
-import img5 from "../../assets/images/img5.png";
-import img6 from "../../assets/images/img6.png";
+import carousel1 from "../../assets/images/carousel/carousel1.png";
+import carousel2 from "../../assets/images/carousel/carousel2.png";
 
-const carouselImageData = [
+export const carouselImageData = [
   {
-    label: "Image 1",
-    alt: "imgage 1",
-    src: img1,
+    label: "carousel 1",
+    alt: "carousel 1",
+    src: carousel1,
   },
   {
-    label: "Image 2",
-    alt: "imgage 2",
-    src: img2,
-  },
-  {
-    label: "Image 3",
-    alt: "imgage 3",
-    src: img3,
-  },
-  {
-    label: "Image 4",
-    alt: "imgage 4",
-    src: img4,
-  },
-  {
-    label: "Image 5",
-    alt: "imgage 5",
-    src: img5,
-  },
-  {
-    label: "Image 6",
-    alt: "imgage 6",
-    src: img6,
+    label: "carousel 2",
+    alt: "carousel 2",
+    src: carousel2,
   },
 ];
-
-export default carouselImageData;
