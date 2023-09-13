639927342efd3bd5057f06fbbd4fac3f35816d34
sue <dldustn2040@gmail.com>
2023-09-13 23:31:49 +0900

    design: gallery page

---
+++ src/pages/Gallery.tsx
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+import { Reset } from "styled-reset";
+import { useState, useEffect } from "react";
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from "../firebase";
+import GalleryModal from "../components/GalleryModal";
+import uploadIconUrl from "../assets/icons/gallery_icon/image_upload_icon.png";
+
+const GalleryWrapper = styled.div`
+  display: flex;
+`;
+
+const SideBar = styled.aside`
+  width: 460px;
+`;
+
+const GalleryBox = styled.div`
+  margin-top: 2rem;
+`;
+
+const GalleryHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const GalleryTitle = styled.p`
+  font: normal normal bold 30px/44px Noto Sans KR;
+  margin-bottom: 20px;
+`;
+
+const UploadButton = styled.button`
+  margin: 0;
+  padding: 0;
+  margin-right: 3rem;
+  background: none;
+  border: none;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const UploadIcon = styled.img`
+  width: 45px;
+  height: 45px;
+`;
+
+const GalleryContainer = styled.div`
+  margin-right: 18px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+const GalleryItem = styled.img`
+  width: 360px;
+  height: 240px;
+  margin-right: 36px;
+  margin-bottom: 20px;
+  object-fit: cover;
+  border-radius: 10px;
+`;
+
+const Gallery = () => {
+  const [imgUrl, setImgUrl] = useState<string>("");
+  const [isModal, setIsModal] = useState(false);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      const imageRef = ref(storage, "img1.png");
+      try {
+        const url = await getDownloadURL(imageRef);
+        setImgUrl(url);
+      } catch {
+        setImgUrl("error");
+      }
+    };
+
+    fetchImage();
+  }, []);
+
+  const isModalChange = () => {
+    setIsModal(!isModal);
+  };
+
+  return (
+    <>
+      <Reset />
+      <GalleryWrapper>
+        <SideBar>Sidebar</SideBar>
+        <GalleryBox>
+          <GalleryHeader>
+            <GalleryTitle>YouCanDoIt 내부 사진</GalleryTitle>
+            <UploadButton type="button" onClick={isModalChange}>
+              <UploadIcon src={uploadIconUrl} alt="img upload icon" />
+            </UploadButton>
+          </GalleryHeader>
+          <GalleryContainer>
+            <GalleryItem src={imgUrl} alt="img1" />
+            <GalleryItem src={imgUrl} alt="img1" />
+            <GalleryItem src={imgUrl} alt="img1" />
+            <GalleryItem src={imgUrl} alt="img1" />
+            <GalleryItem src={imgUrl} alt="img1" />
+            <GalleryItem src={imgUrl} alt="img1" />
+            <GalleryItem src={imgUrl} alt="img1" />
+            <GalleryItem src={imgUrl} alt="img1" />
+          </GalleryContainer>
+        </GalleryBox>
+      </GalleryWrapper>
+
+      {/* Modal */}
+      {isModal && <GalleryModal isModalChange={isModalChange} />}
+    </>
+  );
+};
+
+export default Gallery;
