b5152593f21ee0c5cd4e748a4d49ad3ad2f7a8b8
sue <dldustn2040@gmail.com>
2023-09-15 00:31:35 +0900

    feat: add drag&drop, image preview

--- src/components/GalleryModal.tsx
+++ src/components/GalleryModal.tsx
@@ -1,7 +1,16 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useCallback } from "react";
 import styled from "styled-components";
 import closeIconUrl from "../assets/icons/gallery_icon/image_close_icon.png";
 import uploadIconUrl from "../assets/icons/gallery_icon/image_upload_icon.png";
+
+interface ModalProps {
+  isModalChange: () => void;
+}
+
+interface stylesProps {
+  color?: string;
+  backgroundColor?: string;
+}
 
 const ModalBackground = styled.div`
   position: fixed;
@@ -41,16 +50,25 @@ const CloseButton = styled.button`
 `;
 
 const UploadBox = styled.div`
-  width: 660px;
-  height: 250px;
-  margin-top: 10px;
+  width: 600px;
+  height: 320px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  background-color: rgba(0, 0, 0, 0.02);
   box-shadow: 0px 3px 6px #00000029;
   border: 2px solid #d2d2d2;
   border-radius: 30px;
+  cursor: pointer;
+`;
+
+const ImagePreview = styled.img`
+  width: 600px;
+  height: 320px;
+  object-fit: cover;
+  border-radius: 30px;
+  box-shadow: 0px 3px 6px #00000029;
 `;
 
 const ModalUploadIcon = styled.img`
@@ -70,24 +88,31 @@ const UploadDescription = styled.p`
   }
 `;
 
-const InputBox = styled.div`
+const FileNameBox = styled.div`
   display: flex;
-  margin: 15px auto 0px 35px;
+  margin: 20px auto 0px 70px;
   > svg {
-    margin-right: 5px;
+    margin-right: 3px;
   }
 `;
 
-const ModalButtonBox = styled.div`
+const Input = styled.input`
+  display: none;
+`;
+
+const FileName = styled.p`
+  margin-left: 5px;
+  text-align: center;
+  font-size: 16px;
+  font-weight: 700;
+  font-family: Noto Sans KR;
+`;
+
+const ButtonBox = styled.div`
   margin: auto 20px 20px auto;
 `;
 
-interface ButtonProps {
-  color?: string;
-  backgroundColor?: string;
-}
-
-const StyledButton = styled.button<ButtonProps>`
+const Button = styled.button<stylesProps>`
   height: 45px;
   padding: 6px 16px;
   margin-left: 15px;
@@ -95,63 +120,118 @@ const StyledButton = styled.button<ButtonProps>`
   font-size: 22px;
   font-weight: 700;
   font-family: Noto Sans KR;
-  color: ${props => props.color || "#000"};
-  background-color: ${props => props.backgroundColor || "#fff"};
   border: 1px solid #d2d2d2;
   border-radius: 10px;
   cursor: pointer;
+  color: ${props => props.color || "#000"};
+  background-color: ${props => props.backgroundColor || "#fff"};
+  
+  &:hover {
+    border: 1px solid #087ea4; 
+    color: ${props => (props.color === "#000" ? "#087ea4" : "fff")};
+    background-color: ${props =>
+      props.backgroundColor === "#000" ? "#087ea4" : "#fff"};
 `;
 
-interface ModalProps {
-  isModalChange: () => void;
-}
-
 const GalleryModal = ({ isModalChange }: ModalProps) => {
-  const backgroundRef = useRef<HTMLDivElement>(null);
+  const [selectedFileName, setSelectedFileName] = useState<string | null>(null);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const modalBackgroundRef = useRef<HTMLDivElement>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
 
+  // drag input
   const handleClickBackground = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target === backgroundRef.current) {
+    if (e.target === modalBackgroundRef.current) {
       isModalChange();
     }
   };
+
+  // click input
+  const handleInputFile = () => {
+    if (imageInputRef.current) {
+      imageInputRef.current.click();
+    }
+  };
+
+  // selected image preview
+  const handlePreview = (file: File | undefined) => {
+    if (file) {
+      setSelectedFileName(file?.name || null);
+
+      const reader = new FileReader();
+      reader.onload = event => {
+        if (event.target) {
+          setSelectedImage(event.target.result as string);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleDrag = useCallback(async (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const selectedFile = e.dataTransfer.files[0];
+    setSelectedFileName(selectedFile?.name || null);
+
+    handlePreview(selectedFile);
+  }, []);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    handlePreview(selectedFile);
+  };
+
   return (
-    <ModalBackground onClick={handleClickBackground} ref={backgroundRef}>
+    <ModalBackground onClick={handleClickBackground} ref={modalBackgroundRef}>
       <ModalBox>
         <CloseButton type="button" onClick={isModalChange}>
           <img src={closeIconUrl} alt="close icon" />
         </CloseButton>
 
-        <UploadBox>
-          <ModalUploadIcon src={uploadIconUrl} alt="upload icon" />
-          <UploadDescription>
-            파일 업로드를 위해 클릭하거나 드래그하세요.
-          </UploadDescription>
+        <UploadBox
+          onClick={handleInputFile}
+          onDrop={handleDrag}
+          onDragOver={e => e.preventDefault()}>
+          {selectedImage ? (
+            <ImagePreview src={selectedImage} alt="미리보기" />
+          ) : (
+            <>
+              <ModalUploadIcon src={uploadIconUrl} alt="upload icon" />
+              <UploadDescription>
+                파일 업로드를 위해 클릭하거나 드래그하세요.
+              </UploadDescription>
+            </>
+          )}
         </UploadBox>
 
-        <InputBox>
+        <FileNameBox>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="1em"
-            // eslint-disable-next-line prettier/prettier
             viewBox="0 0 448 512">
             <path d="M364.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z" />
           </svg>
-          <input type="file" accept=".png, .jpg, .jpeg" />
-        </InputBox>
+          <Input
+            type="file"
+            ref={imageInputRef}
+            onChange={handleFileChange}
+            accept=".png,.jpg,.jpeg"
+          />
+          <FileName>{selectedFileName || "이미지.png"}</FileName>
+        </FileNameBox>
 
-        <ModalButtonBox>
-          <StyledButton
+        <ButtonBox>
+          <Button
             color="#000"
             backgroundColor="#fff"
             type="button"
-            // eslint-disable-next-line prettier/prettier
             onClick={isModalChange}>
             Cancel
-          </StyledButton>
-          <StyledButton color="#fff" backgroundColor="#000" type="button">
+          </Button>
+          <Button color="#fff" backgroundColor="#000" type="button">
             OK
-          </StyledButton>
-        </ModalButtonBox>
+          </Button>
+        </ButtonBox>
       </ModalBox>
     </ModalBackground>
   );
