ee1f9d5c6ce40ec6422850cee55e51c2c5dc86e7
sue <dldustn2040@gmail.com>
2023-09-15 18:15:08 +0900

    fix: react version downgrade

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,17 +1,11 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Reset } from "styled-reset";
 import GallerySection from "../components/gallery/GallerySection";
 import GalleryModal from "../components/gallery/GalleryModal";
 
 const GalleryWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-`;
-
-const SideBar = styled.aside`
-  width: 20%;
-  margin-right: 10px;
 `;
 
 const Gallery = () => {
@@ -23,9 +17,9 @@ const Gallery = () => {
 
   return (
     <GalleryWrapper>
-      <SideBar>Sidebar</SideBar>
-      <GallerySection isModalChange={isModalChange} />
+      {/* 사이드바 컴포넌트 */}
 
+      <GallerySection isModalChange={isModalChange} />
       {/* Modal */}
       {isModal && <GalleryModal isModalChange={isModalChange} />}
     </GalleryWrapper>
