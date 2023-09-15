79533db0ebbf5d2f2bab9d0e1baabf7ebb40c635
Eojoonhyuk <anftlcb@gmail.com>
2023-09-15 18:14:30 +0900

    feat: add gallery and calendar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,51 @@
 import styled from "styled-components";
-import HomeCarousel from "../components/home/HomeCarousel";
+import { HomeCalendar, HomeCarousel, HomeGallery } from "../components/home";
 
-const HomeWrap = styled.div`
+const HomeWrap = styled.main`
   width: 100%;
-  height: calc(100vh - 10rem);
+  height: 83vh;
+`;
+
+const HomeMainSection = styled.section`
+  width: 100%;
+  height: 43vh;
+  display: flex;
+  gap: 4rem;
+  padding: 1.5rem 4rem;
+`;
+
+const HomeNoticeSection = styled.section`
+  height: 7vh;
+  padding: 0 4rem;
+`;
+
+const NoticeSectionContents = styled.div`
+  border-top: 1.2px solid #d2d2d2;
+  align-items: center;
+  display: flex;
+  height: 100%;
+`;
+
+const NoticeTitle = styled.h3`
+  font-size: 0.8rem;
+  font-family: "NotoSansKR-Bold";
 `;
 
 const Home = () => {
   return (
     <HomeWrap>
-      <HomeCarousel />
+      <section>
+        <HomeCarousel />
+      </section>
+      <HomeMainSection>
+        <HomeGallery />
+        <HomeCalendar />
+      </HomeMainSection>
+      <HomeNoticeSection>
+        <NoticeSectionContents>
+          <NoticeTitle>공지사항</NoticeTitle>
+        </NoticeSectionContents>
+      </HomeNoticeSection>
     </HomeWrap>
   );
 };
