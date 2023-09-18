664417aa490d3e8254e51fdfe1f36b27e118ac2d
Eojoonhyuk <anftlcb@gmail.com>
2023-09-18 21:18:38 +0900

    feat: add HomeNotice component

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -2,3 +2,4 @@ export { default as HomeGallery } from "../home/HomeGallery";
 export { default as HomeCarousel } from "../home/HomeCarousel";
 export { default as HomeCalendar } from "../home/HomeCalendar";
 export { default as HomeCalendarModal } from "../home/HomeCalendarModal";
+export { default as HomeNotice } from "../home/HomeNotice";
