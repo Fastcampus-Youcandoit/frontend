6537d5bf30f29ea4aad2fd0cad80ee37ae27859a
Eojoonhyuk <anftlcb@gmail.com>
2023-09-19 22:33:56 +0900

    feat: calendar 일정 추가 삭제 수정기능 구현

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,5 +1,5 @@
 export { default as HomeGallery } from "../home/HomeGallery";
 export { default as HomeCarousel } from "../home/HomeCarousel";
-export { default as HomeCalendar } from "../home/HomeCalendar";
-export { default as HomeCalendarModal } from "../home/HomeCalendarModal";
+export { default as HomeCalendar } from "./Calendar";
+export { default as HomeCalendarModal } from "./AddEventModal";
 export { default as HomeNotice } from "../home/HomeNotice";
