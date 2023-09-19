f95eec1f0ed516722908ab80d84c4fa10a7e5df4
Eojoonhyuk <anftlcb@gmail.com>
2023-09-19 22:43:55 +0900

    refactor: return 코드 수정

--- src/pages/Wiki.tsx
+++ src/pages/Wiki.tsx
@@ -4,13 +4,7 @@ import { Wikis } from "../components/SideBar/SideBar";
 import GlobalStyle from "../styles/globalStyle";
 
 const Wiki = () => {
-  return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Header />
-      <Wikis />
-    </BrowserRouter>
-  );
+  return <div>wikipage</div>;
 };
 
 export default Wiki;
