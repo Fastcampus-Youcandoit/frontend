ca6780e77253a629d73df0e4d3fd68cb4c5bc466
Eojoonhyuk <anftlcb@gmail.com>
2023-09-21 00:54:20 +0900

    Resolved import error

--- src/pages/Wiki.tsx
+++ src/pages/Wiki.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Outlet } from "react-router-dom";
-import { WikiSideBar } from "../components/sidebar/SideBar";
+import { WikiSideBar } from "../components/sideBar/SideBar";
 import Footer from "../components/common/Footer";
 
 const WikiWrapper = styled.div`
