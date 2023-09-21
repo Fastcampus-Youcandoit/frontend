926badf191a50e4fcadc28503cc56cd98d6a45cf
sue <dldustn2040@gmail.com>
2023-09-22 05:03:05 +0900

    design: update commuteModal

--- src/components/commute/CommuteModal.tsx
+++ src/components/commute/CommuteModal.tsx
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { doc, deleteDoc, setDoc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { deleteDoc, doc, setDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import closeIconUrl from "../../assets/icons/gallery_icon/image_close_icon.png";
 import { db } from "../../firebase";
+import { CloseButton } from "../gallery/GalleryModal";
 import {
+  Buttons,
+  Clock,
+  CurrentTime,
+  CurrentTimeLayout,
+  HeaderWrapper,
+  Header,
   Modal,
   ModalLayout,
-  Header,
-  TodayDate,
-  CurrentTimeLayout,
-  CurrentTime,
-  Clock,
-  Buttons,
-  WorkOnOff,
-  OkayButtonDiv,
   OkayButton,
+  OkayButtonDiv,
+  TodayDate,
+  WorkOnOff,
 } from "./StyleComponentCommute";
 
 // props type 설정
@@ -122,8 +125,16 @@ const CommuteModal: React.FC<CommuteModalProps> = ({
   return (
     <ModalLayout>
       <Modal>
-        <Header>출&middot;퇴근</Header>
-        <TodayDate> {formattedDate}</TodayDate>
+        <HeaderWrapper>
+          <div>
+            <Header>출&middot;퇴근</Header>
+            <TodayDate> {formattedDate}</TodayDate>
+          </div>
+          <CloseButton type="button" onClick={handleOkayButton}>
+            <img src={closeIconUrl} alt="close icon" />
+          </CloseButton>
+        </HeaderWrapper>
+
         <CurrentTimeLayout>
           <CurrentTime>
             {workonoff && <span>On</span>}
@@ -139,7 +150,20 @@ const CommuteModal: React.FC<CommuteModalProps> = ({
           </Buttons>
         </CurrentTimeLayout>
         <OkayButtonDiv>
-          <OkayButton onClick={handleOkayButton} workonoff={workonoff}>
+          <OkayButton
+            onClick={handleOkayButton}
+            workonoff={workonoff}
+            color="#000"
+            $borderColor="#d2d2d2"
+            $backgroundColor="#fff">
+            Cancel
+          </OkayButton>
+          <OkayButton
+            onClick={handleOkayButton}
+            workonoff={workonoff}
+            color="#fff"
+            $borderColor="#000"
+            $backgroundColor="#000">
             OK
           </OkayButton>
         </OkayButtonDiv>
