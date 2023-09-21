1b4d59f528858c00f473cabedd8fd0159564e06a
Eojoonhyuk <anftlcb@gmail.com>
2023-09-21 19:31:45 +0900

    docs: notice, home types ts파일 추가

---
+++ src/types/home.ts
@@ -0,0 +1,37 @@
+export type EventData = {
+  id: string;
+  date: string;
+  title: string;
+};
+
+export interface CalendarModalProps {
+  isModalChange: () => void;
+  handleFatchEvent: () => void;
+}
+
+export interface CalendarDetailModalProps {
+  isModalChange: () => void;
+  selectedDay: string;
+  handleFatchEvent: () => void;
+}
+
+export interface SelectEvents {
+  id: string;
+  date: string;
+  title: string;
+  eventId: string;
+}
+
+export interface ImageFilesType {
+  business: string[];
+  jobPosting: string[];
+  officePhoto: string[];
+}
+
+export interface NoticeType {
+  id: string;
+  title: string;
+  date: string;
+  content: string;
+  author: string;
+}
